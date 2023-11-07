879fe554ffdeb77e6cb9502e36768cb2904c543d
Mukhammad Bakhriddinov <salahbm.001@gmail.com>
2023-11-07 16:48:35 +0900

    assets

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,20 @@
-import React from "react";
-
+import { Outlet, Navigate } from "react-router-dom";
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to={"/"} />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img src="" />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
