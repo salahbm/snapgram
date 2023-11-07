4b3f54a9ab4ae1bbb10ce06339a962f057d314c7
Mukhammad Bakhriddinov <salahbm.001@gmail.com>
2023-11-07 16:43:27 +0900

    shadcn

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { Button } from "@/components/ui/button";
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default SignUpForm;
