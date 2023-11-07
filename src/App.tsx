2b77eec6168ea16dd66634105d875d81db76b641
Mukhammad Bakhriddinov <salahbm.001@gmail.com>
2023-11-07 16:21:11 +0900

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./globals.css";
+import SignInForm from "../src/_auth/forms/SignInForm";
+import Home from "../src/_root/pages/Home";
 const App = () => {
-  return <div className="text-white text-6xl underline">App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SignInForm />} />
+        {/* private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
