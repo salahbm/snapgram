5697fa7dc0c3d94a51afe021e98af0fb1e91b535
salahbm <salahbm.001@gmail.com>
2023-11-10 19:17:43 +0900

    home components are added

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,20 @@
+import BottomBar from "@/components/shared/BottomBar";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import TopBar from "@/components/shared/TopBar";
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  return (
+    <div className="md:flex w-full">
+      <TopBar />
+      <LeftSideBar />
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+      <BottomBar />
+    </div>
+  );
 };
 
 export default RootLayout;
