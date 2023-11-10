7c727016a88e3dee11f76db1dd55d6d3dda77e15
salahbm <salahbm.001@gmail.com>
2023-11-10 20:07:01 +0900

    home: pages are created

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ import AuthLayout from "../src/_auth/AuthLayout";
 import { Home } from "../src/_root/pages/index";
 import RootLayout from "../src/_root/RootLayout";
 import { Toaster } from "./components/ui/toaster";
+import Explore from "./_root/pages/Explore";
+import Saved from "./_root/pages/Saved";
+import AllUsers from "./_root/pages/AllUsers";
+import CreatePosts from "./_root/pages/CreatePosts";
+import UpdatePost from "./_root/pages/UpdatePost";
+import PostDetails from "./_root/pages/PostDetails";
+import Profile from "./_root/pages/Profile";
+import UpdateProfile from "./_root/pages/UpdateProfile";
 const App = () => {
   return (
     <main className="flex h-screen">
@@ -18,6 +26,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePosts />} />
+          <Route path="/update-post/:id" element={<UpdatePost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
